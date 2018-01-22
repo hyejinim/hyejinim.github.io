b0a9bbb48426da4e4186fb258dffa926ed8013f0
hyejinim <hyejinim17@gmail.com>
2018-01-22 00:17:26 -0500

    Update Works

---
+++ script.js
@@ -0,0 +1,33 @@
+function openLearned() {
+    var x = document.getElementById("learned");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function openList() {
+    var x = document.getElementById("projects");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied!";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy email";
+}
